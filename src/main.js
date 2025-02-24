66e048657b7f8284f6ce32f6b1c5feb4cce04490
StaratBoard <2847606770@qq.com>
2025-02-24 18:48:20 +0800

    修复插入功能，更新样式代码的位置

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import './style.css'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import './styles/common.css'
 
 createApp(App).mount('#app')
